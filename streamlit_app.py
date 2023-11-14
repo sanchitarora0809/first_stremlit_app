c43071bfc15e770a5e881a9988ab98d3a9329e51
Sanchit arora <78254889+sanchitarora0809@users.noreply.github.com>
2023-11-14 13:01:51 +0530

    Update streamlit_app.py
    new emoji and item added

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit ;
 streamlit.title('My Parents New Healthier Diner')
 
 streamlit.header('Breakfast')
-streamlit.text('Omega 3 & blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text(' 🥣 Omega 3 & blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avacado Toast')
