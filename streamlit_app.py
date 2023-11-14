7e8ac81b1abb2667a842ba83c85983df28f3e0fa
Sanchit arora <78254889+sanchitarora0809@users.noreply.github.com>
2023-11-14 12:59:43 +0530

    Update streamlit_app.py
    text added into the board

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,6 @@ import streamlit ;
 streamlit.title('My Parents New Healthier Diner')
 
 streamlit.header('Breakfast')
+streamlit.text('Omega 3 & blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
