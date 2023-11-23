b94b54c8274994129bb29bae02f92dd84e0fdb4d
Sanchit arora <78254889+sanchitarora0809@users.noreply.github.com>
2023-11-23 14:45:45 +0530

    Update streamlit_app.py
    meta data test

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,9 @@
 import streamlit ;
 import pandas ;
 import snowflake.connector;
+
+
+
 streamlit.title('My Parents New Healthier Diner')
 
 streamlit.header('Breakfast')
@@ -31,4 +34,11 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
 
